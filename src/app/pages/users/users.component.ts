8453db42b7412caca0dd5c03d8458fe295bf1329
fsilvaso <fsilvaso@indracompany.com>
2024-04-26 15:10:38 -0300

    Parâmetros em rotas no Angular com routerLink

--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,14 +1,15 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
-  nomes:string[] = ['Abda', 'Baasa', 'Cadmiel', 'Dafne'];
+  users:string[] = ['Abda', 'Baasa', 'Cadmiel', 'Dafne'];
 
 }
