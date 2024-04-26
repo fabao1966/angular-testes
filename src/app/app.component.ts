f35a8167364fad0a2e584bf56341d3715d778884
fsilvaso <fsilvaso@indracompany.com>
2024-04-26 09:34:09 -0300

    Add rotas no Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { FooterComponent } from "./_components/footer/footer.component";
@@ -12,7 +12,7 @@ import { UsersComponent } from "./pages/users/users.component";
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule, BaseUIComponent, UsersComponent]
+    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule, BaseUIComponent, UsersComponent, RouterModule]
 })
 export class AppComponent {
   title = 'Angular Testes';
