b28a78032cda54e8ad4a23fc8929f8c478f30dd1
fsilvaso <fsilvaso@indracompany.com>
2024-04-26 08:30:18 -0300

    add component baseui e page users

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,14 @@ import { NavbarComponent } from "./_components/navbar/navbar.component";
 import { HomeComponent } from "./pages/home/home.component";
 import { FooterComponent } from "./_components/footer/footer.component";
 import { CommonModule } from '@angular/common';
+import { BaseUIComponent } from "./_components/base-ui/base-ui.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule]
+    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule, BaseUIComponent]
 })
 export class AppComponent {
   title = 'Angular Testes';
