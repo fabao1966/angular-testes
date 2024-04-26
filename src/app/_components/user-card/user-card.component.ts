df3e3264f8f045dd06d67fed1999fbfa7d887083
fsilvaso <fsilvaso@indracompany.com>
2024-04-26 16:20:02 -0300

    usando o Input no Angular

---
+++ src/app/_components/user-card/user-card.component.ts
@@ -0,0 +1,14 @@
+import { User } from './../../_models/user';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-user-card',
+  standalone: true,
+  imports: [],
+  templateUrl: './user-card.component.html',
+  styleUrl: './user-card.component.css'
+})
+export class UserCardComponent {
+  @Input()
+  user!: User;
+}
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,15 +1,36 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserCardComponent } from "../../_components/user-card/user-card.component";
+import { User } from '../../_models/user';
 
 @Component({
-  selector: 'app-users',
-  standalone: true,
-  imports: [CommonModule, RouterLink],
-  templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+    selector: 'app-users',
+    standalone: true,
+    templateUrl: './users.component.html',
+    styleUrl: './users.component.css',
+    imports: [CommonModule, RouterLink, UserCardComponent]
 })
 export class UsersComponent {
-  users:string[] = ['Abda', 'Baasa', 'Cadmiel', 'Dafne'];
+ // users:string[] = ['Abda', 'Baasa', 'Cadmiel', 'Dafne'];
+users: User[] = [
+  {
+    nome: 'Abda',
+    idade: 25
+  },
+  {
+    nome: 'Baasa',
+    idade: 28
+  },
+  {
+    nome: 'Cadmiel',
+    idade: 20
+  },
+  {
+    nome: 'Dafne',
+    idade: 37
+  }
+];
+
 
 }
