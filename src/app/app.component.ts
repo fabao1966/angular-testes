271a96feb0b06bb2a26fb134b50a7cd46b8694a5
fsilvaso <fsilvaso@indracompany.com>
2024-04-26 08:45:12 -0300

    Como usar o NgFor do Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,13 +5,14 @@ import { HomeComponent } from "./pages/home/home.component";
 import { FooterComponent } from "./_components/footer/footer.component";
 import { CommonModule } from '@angular/common';
 import { BaseUIComponent } from "./_components/base-ui/base-ui.component";
+import { UsersComponent } from "./pages/users/users.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule, BaseUIComponent]
+    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule, BaseUIComponent, UsersComponent]
 })
 export class AppComponent {
   title = 'Angular Testes';
--- src/app/pages/users/users.component.ts
+++ src/app/pages/users/users.component.ts
@@ -1,12 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './users.component.html',
   styleUrl: './users.component.css'
 })
 export class UsersComponent {
+  nomes:string[] = ['Abda', 'Baasa', 'Cadmiel', 'Dafne'];
 
 }
