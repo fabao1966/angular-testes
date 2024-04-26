f739131451a8f719720f617f92f8a4d1c62f4655
fsilvaso <fsilvaso@indracompany.com>
2024-04-26 08:12:05 -0300

    commit inicial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from "./_components/navbar/navbar.component";
+import { HomeComponent } from "./pages/home/home.component";
+import { FooterComponent } from "./_components/footer/footer.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, NavbarComponent, HomeComponent, FooterComponent, CommonModule]
 })
 export class AppComponent {
-  title = 'angular-teste';
+  title = 'Angular Testes';
 }
---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, DoCheck } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent implements DoCheck {
+
+  num:number=1;
+
+  ngDoCheck(){
+    console.log("Mudança detectada...");
+  }
+
+  adiciona(){
+    this.num++;
+  }
+
+}
